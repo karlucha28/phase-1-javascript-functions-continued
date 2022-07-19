192b4affeb48b2325160312f2427318ac6563902
Karl Savinskiy <karlsavinskiy@gmail.com>
2022-07-19 04:43:51 -0700

    need to be finished

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(parametr = "special"){
+    if (parametr = "*") {
+        return function() {
+            console.log(`You are ${parametr}a hard worker${parametr}!`)
+    }
+    } else if (parametr = "||") {
+        return function() {
+            console.log(`You are ${parametr}a hard worker${parametr}!`)
+    }
+    }
+}
